31630ed544229ced974b590a2c694cbfefaef52d
Evgenii Demidov <kofesutra@gmail.com>
2022-08-23 16:12:50 +0300

    Скорректировал правила игры и сделал текст прокручиваемым

--- app/src/main/java/ru/kofesutra/fiveten_v2/presentation/ui/howtoplay/HowToPlayFragment.kt
+++ app/src/main/java/ru/kofesutra/fiveten_v2/presentation/ui/howtoplay/HowToPlayFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import ru.kofesutra.fiveten_v2.R
 import ru.kofesutra.fiveten_v2.databinding.FragmentHowToPlayBinding
 
 class HowToPlayFragment : Fragment() {
@@ -30,24 +28,28 @@ class HowToPlayFragment : Fragment() {
 
         val rules = """
             Правила игры:
-            На один ход даётся три броска костей.
-            "5" считается как пять очков, "1" считается как десять очков, другие грани не имеют значения.
-            Кубики с невыпавшими "5" и "1" перебрасываются.
-            Кубики бросаются игроками по очереди.
-            Игроки делают одинаковое число ходов.
-            Побеждает тот игрок, который раньше наберёт 100 очков или, в случае, если все набрали 100, у кого общий итог больше.
-                                    
-            PS. Эту игру я когда-то придумал сам :)"
+            
+            - На один ход даётся три броска костей.
+            - "5" считается как пять очков, "1" считается как десять очков, другие грани не имеют значения.
+            - Кубики с невыпавшими "5" и "1" перебрасываются.
+            - Кубики бросаются игроками по очереди.
+            - Игроки делают одинаковое число ходов.
+            - Побеждает тот игрок, который раньше наберёт 100 очков или, в случае, если все набрали 100, у кого общий итог больше.
+            
+            Как играть:
+            
+            - Нажмите кнопку "БРОСОК!"
+            - Если во время следующего броска желаете перебросить какие либо кости выделите их нажатием
+            - И снова кнопка "БРОСОК!"
+            - После третьего броска дайте сыграть Андроиду
+            
+            PS. Эту игру я когда-то придумал сам
+            :)
+              
+                            
         """.trimIndent()
 
         binding?.howtoText?.text = rules
-    }
-
-    override fun onStart() {
-        super.onStart()
-        binding?.howBtnBack?.setOnClickListener {
-            findNavController().navigate(R.id.action_howToPlayFragment_to_mainFragment)
-        }
     }
 
     override fun onDestroy() {
